1264aa113e32a7a1fea3cbb46756f8dffa9d5085
YCTechDev <yctech@yellowchalk.com>
2023-02-14 09:56:20 +0530

    Server Initial SetUp Done

---
+++ Server/Constants/dbconnect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = () => {
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then((data) => {
+        console.log(`MongoDB Connected with server: ${data.connection.host}`)
+    })
+    .catch((error) => console.log(error))
+}
+
+
+module.exports = connectToDatabase;
---
+++ Server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 5000
+
+
+
+
+
+
+
