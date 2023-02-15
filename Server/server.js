c0573cd88e0cb361b3139d5806d34d0385573742
YCTechDev <yctech@yellowchalk.com>
2023-02-15 09:02:16 +0530

    Database connection done

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 5000
+const port = process.env.PORT || 5000;
+const connectToDatabase = require('./Constants/dbconnect');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const { default: mongoose } = require('mongoose');
+
+// Routes Import
 
 
 
 
 
+app.use(cors());
+app.use(morgan());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+
+// connection to  database
+
+mongoose.set("strictQuery",true)
+connectToDatabase();
+
+
+app.listen(port,() => {
+    console.log(`Server is running on PORT ${port}`)
+})
 
 
