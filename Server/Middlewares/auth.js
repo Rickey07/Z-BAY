119c7b4863a25dda6e35a5dd89eb7fd2118e909c
YCTechDev <yctech@yellowchalk.com>
2023-02-17 22:38:22 +0530

    Authentication Middlewares and Controllers Added

---
+++ Server/Middlewares/auth.js
@@ -0,0 +1,32 @@
+const expressJwt = require('express-jwt');
+
+exports.isSignedIn = expressJwt.expressjwt({
+    secret:process.env.JWT_SECRET,
+    algorithms:["HS256"],
+    requestProperty:"auth"
+})
+
+exports.isAuthenticated  = (req,res,next) => {
+    // Check if the User ID and Auth ID are same.
+    let checker =  req.user &&  req.auth && req.user._id === req.auth._id;
+    if(!checker) {
+        return res.status(403).json({
+            error:"Access Denied",
+            statusCode:403,
+            success:false
+        })
+    }
+    next();
+}
+
+exports.isAdmin = (req,res,next) => {
+    // Check if the User is Admin or not
+    if(req.user.role === 0) {
+        return res.status(403).json({
+            error:"Access Denied",
+            statusCode:403,
+            success:false
+        })
+    }
+    next();
+}
---
+++ Server/controllers/auth.js
@@ -0,0 +1,112 @@
+const User = require("../models/user");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+
+exports.signOut = (req, res) => {
+  res.clearCookie("token");
+  res.send("User Successfully signedOut");
+};
+
+exports.signUp = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ errors: errors.array(), statusCode: 400, success: false });
+  }
+
+  try {
+    if (await User.findOne({ email: req.body.email }))
+      return res.status(400).json({
+        error: "User Already exists with this email",
+        statusCode: 400,
+        success: false,
+      });
+    const user = new User(req.body);
+    const newUser = await user.save();
+    if (!Object.values(newUser).length)
+      return res.status(500).json({
+        error: "Internal Server Error Occured! Please Try again Later",
+        statusCode: 500,
+        success: false,
+      });
+    return res.status(200).json({
+      message: "You're now successfully registered! Please Login",
+      statusCode: 200,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+exports.signIn = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ errors: errors.array(), statusCode: 400, success: false });
+  }
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+
+    // Check if the User Exits with given Email Id or Not
+    if (!user) {
+      return res.status(400).json({
+        error: "User Account Not Found with Given Email Id",
+        statusCode: 400,
+        success: false,
+      });
+    }
+
+    // Check  if the email and password is correct
+    if (!user.authenticate(password)) {
+      return res
+        .status(401)
+        .json({
+          error: "Email and password doesnt't match",
+          statusCode: 401,
+          success: false,
+        });
+    }
+
+    // Create  Token
+    const token = jwt.sign(
+      { _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+      function (err) {
+        console.log(err);
+      }
+    );
+    console.log(token);
+
+    // Put Token In Cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    // Send response to the front-end
+    const { _id, name, role } = user;
+    return res.status(200).json({
+      userDetails: {
+        token,
+        name,
+        _id,
+        email: user.email,
+        role,
+      },
+      statusCode: 200,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({
+        error: "Internal Server Error Occured",
+        statusCode: 500,
+        success: false,
+      });
+  }
+};
