8b03efc737fcb80e7879a206f2e20e5b44554fb6
Shania <shaniaantezana@gmail.com>
2023-02-28 16:24:57 -0300

    create new route signIn in auth.routes.js

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const { signUp } = require("../controllers/auth");
+const { signUp, signIn } = require("../controllers/auth");
 const router = Router();
 
 router.post('/signUp',signUp)
+router.post('/signIn',signIn)
 
 module.exports = router
