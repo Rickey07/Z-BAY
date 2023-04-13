2dee296395ea9d8cccbfec321b61de2a4974c21d
YCTechDev <yctech@yellowchalk.com>
2023-04-13 14:53:34 +0530

    Products Layout Completed Responsiveness Reamining

--- Client/src/Components/SideBar/SidebarForFilter.jsx
+++ Client/src/Components/SideBar/SidebarForFilter.jsx
@@ -11,34 +11,108 @@ import {
   Checkbox,
   Paper,
   Typography,
+  Rating,
+  Divider,
 } from "@mui/material";
 import { useSelector } from "react-redux";
 
 const SidebarForFilter = () => {
   const theme = useTheme();
-  const categories = useSelector((state) => state.category.categories.categories);
+  const categories = useSelector(
+    (state) => state.category.categories.categories
+  );
 
   const sideBarFilterStyles = {
-    mainPaperBox:{
-      padding:"15px"
+    mainPaperBox: {
+      padding: "15px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px",
     },
-
-  }
+    sideBarFilterSubItem: {
+      color: "rgb(125, 135, 156)",
+    },
+    sideBarDivider: {
+      borderColor: "rgb(243, 245, 249)",
+      borderWidth: "0px 0px thin",
+    },
+  };
   console.log(categories);
 
   return (
     <>
-     <Paper component={"div"} style={sideBarFilterStyles.mainPaperBox} elevation={1}>
+      <Paper
+        component={"div"}
+        style={sideBarFilterStyles.mainPaperBox}
+        elevation={1}
+      >
         <Box component={"div"}>
-          <Typography variant="p" sx={{textAlign:"start"}}>Categories</Typography>
+          <Typography
+            component={"h6"}
+            variant="p"
+            sx={{ textAlign: "start", fontSize: "18px" }}
+          >
+            Categories
+          </Typography>
           <FormGroup>
             {categories?.map((category) => {
-              return <FormControlLabel key={category?._id} control={<Checkbox/>} label={category?.category_name}/>
-
+              return (
+                <FormControlLabel
+                  key={category?._id}
+                  control={<Checkbox />}
+                  style={sideBarFilterStyles.sideBarFilterSubItem}
+                  label={category?.category_name}
+                />
+              );
             })}
           </FormGroup>
+          <Divider style={sideBarFilterStyles.sideBarDivider} />
         </Box>
-     </Paper>
+        <Box component={"div"}>
+          <Typography
+            component={"h6"}
+            variant="p"
+            sx={{ textAlign: "start", fontSize: "18px" }}
+          >
+            Sort By: Price
+          </Typography>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="LTH"
+                control={<Radio />}
+                sx={sideBarFilterStyles.sideBarFilterSubItem}
+                label="Low To High"
+              />
+              <FormControlLabel value="HTL" control={<Radio />} sx={sideBarFilterStyles.sideBarFilterSubItem} label="High to Low" />
+            </RadioGroup>
+          </FormControl>
+          <Divider style={sideBarFilterStyles.sideBarDivider}/>
+        </Box>
+        <Box component={"div"}>
+          <Typography component={"h6"}
+            variant="p"
+            sx={{ textAlign: "start", fontSize: "18px" }}>Rating</Typography>
+                      <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              
+              <FormControlLabel value="4andabove" control={<Radio />} label={<Rating value={4} readOnly/>} />
+              <FormControlLabel value="3andabove" control={<Radio />} label={<Rating value={3} readOnly/>} />
+              <FormControlLabel value="2andabove" control={<Radio />} label={<Rating value={2} readOnly/>} />
+              <FormControlLabel value="1andabove" control={<Radio />} label={<Rating value={1} readOnly/>} />
+
+            </RadioGroup>
+          </FormControl>  
+        </Box>
+      </Paper>
     </>
   );
 };
--- Client/src/Pages/Products.jsx
+++ Client/src/Pages/Products.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Product from "../Components/Product/Product.jsx";
-import { Grid } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  InputBase,
+} from "@mui/material";
 import SidebarForFilter from "../Components/SideBar/SidebarForFilter.jsx";
 
 const Products = () => {
@@ -103,7 +110,7 @@ const Products = () => {
       description:
         "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
       category: "Clothes",
-      isInWishList:true,
+      isInWishList: true,
       image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
       rating: {
         rate: 3.6,
@@ -128,32 +135,89 @@ const Products = () => {
     },
   ];
 
+  const MainSearchBar = () => {
+    const mainSearchBarstyles = {
+      mainContainerWrapper: {
+        padding: "3px 50px",
+        borderRadius: "8px",
+      },
+      mainContainer: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
+      inputContainer: {
+        maxWidth:"700px",
+        width:"100%"
+      },
+      input: {
+        borderRadius: "16px",
+      },
+    };
+
+    return (
+      <>
+        <Paper
+          component={"div"}
+          style={mainSearchBarstyles.mainContainerWrapper}
+          elevation={1}
+        >
+          <Box component={"div"} sx={mainSearchBarstyles.mainContainer}>
+            <Box component={"div"} sx={mainSearchBarstyles.inputContainer}>
+              {/* <TextField style={mainSearchBarstyles.input} fullWidth label="fullWidth" id="fullWidth" />  */}
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search For Products"
+                inputProps={{ "aria-label": "search google maps" }}
+                fullWidth
+                // style={{border:"1px solid red"}}
+              />
+            </Box>
+            <Box>
+              <Typography component={"h6"} variant="h6">
+                Searching For
+              </Typography>
+              <Typography component={"h6"} variant={"p"}>
+                48 Results Found
+              </Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </>
+    );
+  };
+
   return (
     <>
       <div>Products</div>
-      <Grid container>
-        <Grid item md={2}>
+      <Box display={"flex"} padding={"50px"} flexDirection={"column"} gap={"2rem"}> 
+      <MainSearchBar />
+      <Grid container spacing={3}>
+        <Grid item md={2} xs={10}>
           <SidebarForFilter />
+        </Grid>
+        <Grid item md={10}>
+          <Grid container spacing={3}>
+            {productsData.map((product) => {
+              return (
+                <Grid item md={4} xs={12} key={product.id}>
+                   <Product
+                  title={product.title}
+                  price={product.price}
+                  quantity={product.quantity}
+                  category={product.category}
+                  image_url={product.image}
+                  OffPercentage={product.OffPercentage}
+                  rating={product.rating}
+                  isInWishList={product.isInWishList}
+                />
+                 </Grid>
+              );
+            })}
           </Grid>
-          <Grid item md={10}>
-            <Grid container columnGap={3} rowGap={3}>
-              {productsData.map((product) => {
-                return (
-                  <Product
-                    title={product.title}
-                    price={product.price}
-                    quantity={product.quantity}
-                    category={product.category}
-                    image_url={product.image}
-                    OffPercentage={product.OffPercentage}
-                    rating={product.rating} 
-                    isInWishList={product.isInWishList}
-                  />
-                );
-              })}
-            </Grid>
         </Grid>
       </Grid>
+      </Box>
     </>
   );
 };
