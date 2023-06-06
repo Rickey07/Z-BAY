91e3d5e940e21397ce3321263845308ef34b4669
Rickey07 <Rickeyrickey747@gmail.com>
2023-06-06 14:38:12 +0530

    File changed for deployment

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,8 @@ const analyticsRoutes = require('./routes/analytics')
 // App Common Middlewares
 app.use(cors());
 app.use(morgan());
-app.use("/uploads",express.static('uploads'))
+// app.use("/uploads",express.static('uploads'))
+app.use("/uploads", express.static(__dirname + '/uploads'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
