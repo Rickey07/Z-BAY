b91ed9c6f83b8d45460380137be81e801b8ed6da
Rickey07 <Rickeyrickey747@gmail.com>
2023-06-06 10:35:14 +0530

    URL Changed

--- Client/src/redux/CategoriesSlice.js
+++ Client/src/redux/CategoriesSlice.js
@@ -4,7 +4,7 @@ import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 const fetchAllCategories = createAsyncThunk(
     'categories/getAllCategories',
     async (thunkAPI) => {
-        const res = await fetch('http://localhost:5000/api/category/all');
+        const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/category/all`);
         const data = await res.json();
         return data;
     }
